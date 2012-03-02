057e11b532e7c8943adbaa85f328203d0bfc308e
Bruno Binet <bruno.binet@gmail.com>
2012-03-02 14:35:50 +0100

    first commit: pelican-quickstart

---
+++ pelican.conf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Bruno Binet"
+SITENAME = u"Centre Eben-Ezer"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG='fr'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = False
+
+
+    
