791c172e5c6f45f68a59052861d5c449e64bfbaa
Bruno Binet <bruno.binet@gmail.com>
2012-04-27 02:11:13 +0200

    do not turn the image into a figure if alt="no-figure"

--- theme/static/local.js
+++ theme/static/local.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     // Every image referenced from a Markdown document
     $(".body img, .article img").each(function() {
+
+        // do not turn the image into a figure if alt="no-figure"
+        if($(this).attr("alt") == "no-figure") {return;}
+
         $(this).wrap('<figure style="width:'+(this.width+1)+'px;"></figure>')
         if($(this).attr("alt")) {
             // Let's put a caption if there is one
