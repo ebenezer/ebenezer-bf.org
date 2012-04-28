c054927d0c79ff029707f6e2da70aa6d2a551fc5
Bruno Binet <bruno.binet@gmail.com>
2012-04-28 23:30:05 +0200

    fix script pull_and_ghpimport.sh

--- pull_and_ghpimport.sh
+++ pull_and_ghpimport.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-oldhash=$(git rev-parse refs/heads/gh-pages)
+oldhash=$(git rev-parse HEAD)
 git pull origin master
-newhash=$(git rev-parse refs/heads/gh-pages)
-if [ $oldhash = $newhash ]; then
+newhash=$(git rev-parse HEAD)
+if [ "$oldhash" == "$newhash" ]; then
     echo "Nothing to do."
 else
     echo "Update gh-pages branch and push to github."
