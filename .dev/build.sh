69e4aa9d71cd7c229af9eabb914fba9fe7916d53
Bruno Binet <bruno.binet@gmail.com>
2012-05-12 15:46:09 +0200

    update build.sh script

--- .dev/build.sh
+++ .dev/build.sh
@@ -4,11 +4,20 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 LESSC=./submodules/less.js/bin/lessc
 
 ( cd "${BASEDIR}"
-    $LESSC --compress ./swatchmaker.less > ../static/css/bootstrap.min.css
-    cat ./submodules/galleria/src/galleria.js \
-        ./submodules/galleria/src/plugins/flickr/galleria.flickr.js | \
-        ./submodules/UglifyJS/bin/uglifyjs > ../static/js/galleria.js
 
-    rsync -avz --cvs-exclude ./static/* ../static
-    rsync -avz --cvs-exclude ./submodules/galleria/src/themes/* ../static/galleria-themes
+    echo "> Concatenate and build less/css to theme/static/css/all.min.css"
+    $LESSC --compress ./swatchmaker.less > ./theme/static/css/all.min.css
+
+    echo "> Concatenate and build js to theme/static/js/all.min.js"
+    cat ./static/js/jquery-1.7.1.js \
+        ./submodules/galleria/src/galleria.js \
+        ./submodules/galleria/src/plugins/flickr/galleria.flickr.js \
+        ./static/js/local.js | ./submodules/UglifyJS/bin/uglifyjs \
+        > ./theme/static/js/all.min.js
+
+    echo "> Copy bootstrap/img/ files to theme/static/img/"
+    rsync -avz --cvs-exclude ./submodules/bootstrap/img/* ./theme/static/img
+
+    echo "> Copy galleria/src/themes files to theme/static/galleria-themes/"
+    rsync -avz --cvs-exclude ./submodules/galleria/src/themes/* ./theme/static/galleria-themes
 )
