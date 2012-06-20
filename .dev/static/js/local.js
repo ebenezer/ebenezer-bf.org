bfc9fa0ce279f0fddaac35f4d6cc9f5e8d2e43f5
Bruno Binet <bruno.binet@gmail.com>
2012-06-21 01:12:36 +0200

    update local.js to add support for galleria gallery

--- .dev/static/js/local.js
+++ .dev/static/js/local.js
@@ -15,16 +15,84 @@ $(document).ready(function() {
         }
     });
 
-    $("div.galleria").each(function(index) {
+    var gloaded = false;
+
+    // iterate over div.galleria not in div.gallery
+    $("div.galleria").filter(function(){
+        return !$(this).parents('div').hasClass('gallery');
+    }).each(function(index) {
         // Load the classic theme only once
-        if (index === 0) {
+        if (!gloaded) {
             Galleria.loadTheme(galleria_theme_url);
+            gloaded = true;
         }
         // Initialize Galleria
         $('.galleria').galleria({
             flickr: $(this).attr("id"),
+            flickrOptions: {
+                max: 500
+            },
             responsive: true,
             height: 0.5625
         });
+    });
+
+    $("div.gallery").each(function(index) {
+        // Load the classic theme only once
+        if (!gloaded) {
+            Galleria.loadTheme(galleria_theme_url);
+            gloaded = true;
+        }
+
+        var flickr = new Galleria.Flickr();
+        var gal = $(this).find('div.galleria');
+        var loader = $('<div>', { 'class': 'loader' }).hide().appendTo(this);
+        var set;
+
+        flickr.setOptions({
+            max: 500,
+            description: true
+        });
+
+        // attach event handler for the menu
+        $(this).find('.nav a').not('.dropdown-toggle').click(function(e) {
+
+            e.preventDefault();
+
+            // extract the set id from the link href
+            set = this.href.split('/');
+            set = set[set.length-2];
+
+            if (gal.data('galleria')) {
+                // if galleria has already been initialized: display the loader
+                loader.text('Loading "'+$(this).text()+'"').show();
+            }
+
+            flickr.set(set, function(data) {
+
+                // hide the loader
+                loader.fadeOut('fast');
+
+                if (gal.data('galleria')) {
+                    // if galleria has already been initialized: load the new data
+                    gal.data('galleria').load(data);
+                } else {
+                    // else initialize galleria (1st time)
+                    gal.galleria({
+                        dataSource: data,
+                        responsive: true,
+                        height: 0.5625
+                    });
+                }
+            });
+
+            // update 'active' class
+            $(this).closest('div.gallery').find('.nav li').removeClass('active');
+            $(this).parents('li').addClass('active');
+        });
+
+        // trigger a click onload so that the first gallery will be displayed when entering
+        $(this).find('.nav a').not('.dropdown-toggle').first().click();
+
     });
 });
